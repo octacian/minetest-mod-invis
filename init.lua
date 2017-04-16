3775d9e7b9d1b7cdbe6a2d421e0f4b79a5e9f86b
octacian <enduffy2014@outlook.com>
2017-04-16 11:46:13 -0700

    Add /invisible chatcommand

--- init.lua
+++ init.lua
@@ -1,1 +1,70 @@
 -- invis/init.lua
+
+invis = {}
+local invisible = {}
+
+-- [function] Get visible
+function invis.get(name)
+  if type(name) == "userdata" then
+    name = player:get_player_name()
+  end
+
+  return invisible[name]
+end
+
+-- [function] Toggle invisible
+function invis.toggle(player, toggle)
+  if type(player) == "string" then
+    player = minetest.get_player_by_name(player)
+  end
+
+  local prop, msg
+  local name      = player:get_player_name()
+  invisible[name] = toggle
+
+  if toggle == true then
+    -- Hide player and nametag
+    prop = {
+      visual_size = {x = 0, y = 0},
+      collisionbox = {0, 0, 0, 0, 0, 0},
+    }
+    player:set_nametag_attributes({
+      color = {a = 0, r = 255, g = 255, b = 255},
+    })
+    msg = "now invisible"
+  else
+    -- Show player and nametag
+    prop = {
+			visual_size = {x = 1, y = 1},
+			collisionbox = {-0.35, -1, -0.35, 0.35, 1, 0.35},
+		}
+		player:set_nametag_attributes({
+			color = {a = 255, r = 255, g = 255, b = 255},
+		})
+    msg = "no longer invisible"
+  end
+
+  -- Update properties
+  player:set_properties(prop)
+  -- Return message
+  return name.." is "..msg
+end
+
+-- [register] Privilege
+minetest.register_privilege("invisibility", "Allow use of /invisible command")
+
+-- [register] Command
+minetest.register_chatcommand("invisible", {
+  description = "Make yourself or another player invisible",
+  params = "<name>",
+  privs = {invisibility=true},
+  func = function(name, param)
+    if minetest.get_player_by_name(param) then
+      name = param
+    elseif param ~= "" then
+      return false, "Invalid player \""..param.."\""
+    end
+
+    return true, invis.toggle(name, not invisible[name])
+  end,
+})
